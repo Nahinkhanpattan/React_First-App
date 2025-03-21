c18120e5958567e4e426548fbb6c432f3aaa64e3
Nahin <nahinkhanpattan@gmail.com>
2025-03-21 20:38:27 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./assets/style.css"
+import ProfileCard from "./components/ProfileCard"
+
+export default function App() {
+ return( <>
+  <ProfileCard></ProfileCard>
+  </>
+)}
---
+++ src/components/ProfileCard.jsx
@@ -0,0 +1,17 @@
+export default function ProfileCard() {
+    return (
+        <>
+            <div className="card">
+                <div className="img">
+                    <img src="/vite.svg" alt="" />
+                </div>
+                <div className="name">
+                    <h1>Nahin</h1>
+                </div>
+                <div className="description">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, commodi non quos et velit debitis perspiciatis dolores rem reiciendis aliquid ducimus eveniet, necessitatibus fugit repellat.</p>
+                </div>
+            </div>
+        </>
+    );
+}
