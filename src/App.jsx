9bbcba453ea34a05cae0b6b556d37d662573e0d5
Nahin <nahinkhanpattan@gmail.com>
2025-03-24 17:46:27 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
+import { useState } from "react"
 import "./assets/style.css"
-import ProfileCard from "./components/ProfileCard"
-import TheatreCard from './components/TheatreCard'
-import reactLogo from "./assets/react.svg";
-
+import TheatreCard from "./components/TheatreCard"
 
 export default function App() {
- return( <>
-  {/* <ProfileCard></ProfileCard> */}
-  <div className="cardContainer">
-  <TheatreCard card={{name:"lion king",img:"https://upload.wikimedia.org/wikipedia/en/9/9d/Disney_The_Lion_King_2019.jpg", dis:"After the murder of his father, a young lion prince flees his kingdom only to learn the true meaning of responsibility and bravery.After the murder of his ..."}}></TheatreCard>
-  <TheatreCard card={{name:"godzilla",img:"https://m.media-amazon.com/images/M/MV5BODE2NTdmMmYtY2U1OS00MjExLWIwNjQtYjQ5NTA0ZDZmZjZiXkEyXkFqcGc@._V1_.jpg", dis:"The world is beset by the appearance of monstrous creatures, but one of them may be the only one who can save humanity."}}></TheatreCard>
-  <TheatreCard card={{name:"king kong",img:"https://m.media-amazon.com/images/M/MV5BMWY0NWE0ZjUtNjRlOS00ZDViLTgxNTQtMGMwN2FhNDY1YTg2XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg", dis:"A greedy film producer assembles a team of moviemakers and sets out for the infamous Skull Island, where they find more than just cannibalistic natives."}}></TheatreCard>
-  </div>
-  </>
-)}
+  const URL = "https://www.omdbapi.com/?t="
+  const API = "&apikey=a93a0e38"
+
+  const [movie1, setMovie1] = useState(null)
+  const [movie2, setMovie2] = useState(null)
+  const [movie3, setMovie3] = useState(null)
+
+  const fetchMovie = async (title, setMovie) => {
+    let response = await fetch(`${URL}${title}${API}`)
+    let data = await response.json()
+    setMovie({
+      name: data.Title,
+      img: data.Poster,
+      dis: data.Plot
+    })
+  }
+
+  if (!movie1) {
+    fetchMovie("avengers", setMovie1)
+    return <h2>Loading Avengers...</h2>
+  }
+  if (!movie2) {
+    fetchMovie("godzilla", setMovie2)
+    return <h2>Loading Godzilla...</h2>
+  }
+  if (!movie3) {
+    fetchMovie("king kong", setMovie3)
+    return <h2>Loading King Kong...</h2>
+  }
+
+  return (
+    <>
+      <div className="cardContainer">
+        <TheatreCard card={movie1} />
+        <TheatreCard card={movie2} />
+        <TheatreCard card={movie3} />
+      </div>
+    </>
+  );
+}
